569c9d5906075fd85510cb1e9b49d6bf61bb2f41
𝐑𝐞𝐳𝐚 𝐀𝐬𝐚𝐝𝐢 <149728623+RezaGooner@users.noreply.github.com>
2024-11-27 20:22:51 +0330

    Update B.cpp

--- 2020/B.cpp
+++ 2020/B.cpp
@@ -3,17 +3,21 @@
 using namespace std;
 
 int main() {
-    int n ;
-    cin >> n ;
+    int n;
+    cin >> n;
 
-    int result , temp , entry ;
-    bool isFirst = true ;
-    for (int i = 0 ; i < n ; i++){
-        if (!isFirst) temp = entry ;
-        cin >> entry ;
-        if (isFirst) temp = entry ;
-        isFirst = false ;
-        entry > temp ? result += entry - temp : result += 0 ;
+    int result = 0, prev, current;
+    cin >> prev;
+    
+    for (int i = 1; i < n; i++) {
+        cin >> current;
+
+        if (current > prev) {
+            result += current - prev;
+            current = prev;
+        }
+
+        prev = current;
     }
 
     cout << result << endl ;
