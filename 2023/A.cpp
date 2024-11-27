6bbcef8afcd0ec0558da3c3989125551d795bf13
𝐑𝐞𝐳𝐚 𝐀𝐬𝐚𝐝𝐢 <149728623+RezaGooner@users.noreply.github.com>
2024-11-27 12:37:39 +0330

    Create A.cpp

---
+++ 2023/A.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int referals ;
+    cin >> referals ;
+
+    int percent = referals / 10 ;
+
+    cout << percent << endl ;
+
+    return 0;
+}
