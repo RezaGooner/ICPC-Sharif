c198ce17e9fd5898d8bc3d7a1507e2ca065d894c
𝐑𝐞𝐳𝐚 𝐀𝐬𝐚𝐝𝐢 <149728623+RezaGooner@users.noreply.github.com>
2024-12-04 00:20:32 +0330

    Update D.cpp

--- 2014/D.cpp
+++ 2014/D.cpp
@@ -1,48 +1,78 @@
 #include <iostream>
 #include <vector>
-#include <cmath>
-#include <string>
+#include <queue>
+#include <map>
+#include <climits>
+#include <set>
 
 using namespace std;
 
-struct HTMLColor {
-    string name;
-    int r, g, b;
+struct Edge {
+    int to, weight;
 };
 
-double calculateDistance(int r1, int g1, int b1, int r2, int g2, int b2) {
-    return sqrt(pow(r1 - r2, 2) + pow(g1 - g2, 2) + pow(b1 - b2, 2));
+int dijkstra(int start, int end, const map<int, vector<Edge>>& graph) {
+    map<int, int> dist;
+    for (const auto& [node, _] : graph) {
+        dist[node] = INT_MAX;
+    }
+    dist[start] = 0;
+
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
+    pq.push({0, start});
+
+    while (!pq.empty()) {
+        auto [d, current] = pq.top();
+        pq.pop();
+
+        if (current == end) return d;
+
+        if (d > dist[current]) continue;
+
+        for (const auto& edge : graph.at(current)) {
+            int newDist = d + edge.weight;
+            if (newDist < dist[edge.to]) {
+                dist[edge.to] = newDist;
+                pq.push({newDist, edge.to});
+            }
+        }
+    }
+
+    return -1;
 }
 
 int main() {
-    vector<HTMLColor> colors = {
-        {"White", 255, 255, 255}, {"Silver", 192, 192, 192}, {"Gray", 128, 128, 128},
-        {"Black", 0, 0, 0}, {"Red", 255, 0, 0}, {"Maroon", 128, 0, 0},
-        {"Yellow", 255, 255, 0}, {"Olive", 128, 128, 0}, {"Lime", 0, 255, 0},
-        {"Green", 0, 128, 0}, {"Aqua", 0, 255, 255}, {"Teal", 0, 128, 128},
-        {"Blue", 0, 0, 255}, {"Navy", 0, 0, 128}, {"Fuchsia", 255, 0, 255},
-        {"Purple", 128, 0, 128}
-    };
-
     while (true) {
-        int r, g, b;
-        cin >> r >> g >> b;
+        int n, start, end;
+        cin >> n >> start >> end;
+        if (n == 0 && start == 0 && end == 0) break;
 
-        if (r == -1 && g == -1 && b == -1)
-            break;
+        map<int, vector<Edge>> graph;
+        vector<set<int>> elevatorFloors(n);
 
-        string closestColor;
-        double minDistance = 1e9;
-
-        for (int i = 0; i < colors.size(); i++) {
-            double distance = calculateDistance(r, g, b, colors[i].r, colors[i].g, colors[i].b);
-            if (distance < minDistance) {
-                minDistance = distance;
-                closestColor = colors[i].name;
+        for (int i = 0; i < n; i++) {
+            int m;
+            cin >> m;
+            vector<int> floors(m);
+            for (int j = 0; j < m; j++) {
+                cin >> floors[j];
+                elevatorFloors[i].insert(floors[j]);
             }
+
+            for (int x : floors)
+                for (int y : floors)
+                    if (x != y)
+                        graph[x].push_back({y, abs(x - y)});
         }
 
-        cout << closestColor << endl;
+        for (int i = 0; i < n; i++)
+            for (int j = i + 1; j < n; j++)
+                for (int floor : elevatorFloors[i])
+                    if (elevatorFloors[j].count(floor))
+                        graph[floor].push_back({floor, 0});
+
+        int minTime = dijkstra(start, end, graph);
+        cout << minTime << endl;
     }
 
     return 0;
